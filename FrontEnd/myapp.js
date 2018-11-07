ebc4a2a41224d7abc1ec6ad2d6c9512e6a738889
Dennis J. Small <dsmall1@nd.edu>
2018-11-07 18:56:42 -0500

    t#

--- FrontEnd/myapp.js
+++ FrontEnd/myapp.js
@@ -2,20 +2,19 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', [
   '$scope', '$http', function($scope, $http) {
-    console.log($scope);
+
     $http({
       method: 'GET',
       url: 'http://localhost:3000/food'
     }).then(function successCallback(response) { console.log(response);
         // this callback will be called asynchronously
         // when the response is available
-      }, function errorCallback(error) { console.log(error);
+      $scope.foods=response.data}, function errorCallback(error) { console.log(error);
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-      });
+
+      // console.log($scope);
+    })
     $scope.foods = []
-    $scope.foods[0]={image:'img/hamburger.png',distance:0.2 , rating: 4.1 , name:'Smashburger' }
-
-
-
-}]);
+    $scope.foods[0]={image:'http://localhost:3000/food',distance:0.2 , rating: 4.1 , name:'Smashburger'}
+  }])
